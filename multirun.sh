44ab234ee331c641144fc58b7da4aae158c371dd
Ruben Kruiper <rubenkruiper@gmail.com>
2021-08-11 13:50:03 +0200

    updated experiments

--- multirun.sh
+++ multirun.sh
@@ -1,19 +1,14 @@
-python run_tagger.py -c experiments/dr.00_enc_1_h192_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/00_enc_1_h192_ffnn_1_h200_1e
-wait 
-python run_tagger.py -c experiments/dr.00_enc_1_h384_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.00_enc_1_h384_ffnn_1_h200_1e-4_32
-wait 
-python run_tagger.py -c experiments/dr.01_enc_1_h384_ffnn_1_h200_1e-3_32.json -m /mnt/sda/mwe/dr.01_enc_1_h384_ffnn_1_h200_1e-3_32
-wait 
-python run_tagger.py -c experiments/dr.01_enc_1_h384_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_1_h384_ffnn_1_h200_1e-4_32
-wait
-python run_tagger.py -c experiments/dr.01_enc_1_h384_no_ffnn_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_1_h384_no_ffnn_1e-4_32
-wait
-python run_tagger.py -c experiments/dr.01_enc_1_h786_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_1_h786_ffnn_1_h200_1e-4_32
-wait
-python run_tagger.py -c experiments/dr.01_enc_1_h786_ffnn_2_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_1_h786_ffnn_2_h200_1e-4_32
-wait 
-python run_tagger.py -c experiments/dr.01_enc_2_h192_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_2_h192_ffnn_1_h200_1e-4_32
-wait
-python run_tagger.py -c experiments/dr.01_enc_2_h384_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_2_h384_ffnn_1_h200_1e-4_32
-wait
-python run_tagger.py -c experiments/dr.01_enc_passthrough_ffnn_1_h200_1e-4_32.json -m /mnt/sda/mwe/dr.01_enc_passthrough_ffnn_1_h200_1e-4_32
+#!/bin/sh
+DIR='/experiments/'
+# loop over files in folder experiments
+for FILE in ls "$DIR"*
+do
+	filename=$(basename "$FILE")
+	# if not simple_tagger then run and wait
+	if [["$FILENAME" == "simple_tagger.json"]]
+		wait # do nothing
+	else
+		fname="${filename%.*}"
+		python run_tagger.py -c "$FILE" -m "/mnt/sda/mwe/$fname"
+		wait
+done
