44fee59653aee3a3b9f1a6d50f9e077daf711f54
Ruben Kruiper <rubenkruiper@gmail.com>
2021-08-11 13:56:08 +0200

    fixed typo

--- multirun.sh
+++ multirun.sh
@@ -5,10 +5,11 @@ for FILE in ls "$DIR"*
 do
 	filename=$(basename "$FILE")
 	# if not simple_tagger then run and wait
-	if [["$FILENAME" == "simple_tagger.json"]]
+	if ["$FILENAME" == "simple_tagger.json"]; then
 		wait # do nothing
 	else
 		fname="${filename%.*}"
 		python run_tagger.py -c "$FILE" -m "/mnt/sda/mwe/$fname"
 		wait
+	fi
 done
