64ed57798112ab36497849372af89aa848182c52
Ruben Kruiper <rubenkruiper@gmail.com>
2023-01-13 15:56:04 +0100

    no module lib, passing string to try and solve

--- spar_predictor.py
+++ spar_predictor.py
@@ -11,10 +11,9 @@ from allennlp.commands import main
 
 
 cwd = Path.cwd()
-sys.path.insert(0, cwd.joinpath("SPaR.txt"))
+sys.path.insert(0, str(cwd.joinpath("SPaR.txt")))
 import_module_and_submodules("lib")
 import spar_serving_utils
-
 
 
 class SparPredictor:
